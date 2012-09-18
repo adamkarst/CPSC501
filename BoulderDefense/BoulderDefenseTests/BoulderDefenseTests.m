5e1b0ab1d649f5d6ef4f3d0059df086029b2de59
Brian Harrison <brianharrison@Brians-MacBook-Air.local>
2012-09-18 13:26:20 -0600

    first project

---
+++ BoulderDefense/BoulderDefenseTests/BoulderDefenseTests.m
@@ -0,0 +1,32 @@
+//
+//  BoulderDefenseTests.m
+//  BoulderDefenseTests
+//
+//  Created by Brian Harrison on 2012-09-18.
+//  Copyright (c) 2012 Brian Harrison. All rights reserved.
+//
+
+#import "BoulderDefenseTests.h"
+
+@implementation BoulderDefenseTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in BoulderDefenseTests");
+}
+
+@end
